dc8b6c00f440e680e2c3c76ecabdf9f1bacf4b31
Jhunt <Jhunt646@west-mec.org>
2020-11-02 15:24:01 -0700

    yes

--- Unit 4/Practice/script.js
+++ Unit 4/Practice/script.js
@@ -1,49 +1,49 @@
-let x = 45;
+// let x = 45;
 
-45 + "2" //concatination operator ==452
-
-
-console.log(``);
-
-let firstWord = "Banana";
-let secondWord = "bandana";
-
-console.log(`Banana is less than bandana: ${firstWord.toLowerCase() < secondWord.toLowerCase()}`);
+// 45 + "2" //concatination operator ==452
 
 
-console.log(`5 is greater than 4: ${5 > 4}`);
-console.log(`apple is greater than appear: ${'apple' > 'appear'}`);
-console.log(`Went is greater than west: ${'west' < 'Went'}`);
-console.log(`2 is greater than "12": ${2 > '12'}`);
-console.log(`undefined == null: ${undefined == null}`);
-console.log(`undefined === null ${undefined === null}`);
-console.log(`null == "0": ${null == '0'}`);
+// console.log(``);
 
-if (false == false) {
-    console.log(`it was equal`);
-}
+// let firstWord = "Banana";
+// let secondWord = "bandana";
+
+// console.log(`Banana is less than bandana: ${firstWord.toLowerCase() < secondWord.toLowerCase()}`);
 
 
-let T = true
+// console.log(`5 is greater than 4: ${5 > 4}`);
+// console.log(`apple is greater than appear: ${'apple' > 'appear'}`);
+// console.log(`Went is greater than west: ${'west' < 'Went'}`);
+// console.log(`2 is greater than "12": ${2 > '12'}`);
+// console.log(`undefined == null: ${undefined == null}`);
+// console.log(`undefined === null ${undefined === null}`);
+// console.log(`null == "0": ${null == '0'}`);
 
-if (T == true) {
-    console.log(`its true!`);
-}
-
-if (T == false) {
-    console.log(`its false!`);
-
-}
-
-if (T) console.log(`it was true`);
-if ("") console.log(`this is false`);
+// if (false == false) {
+//     console.log(`it was equal`);
+// }
 
 
-if (T) {
-    console.log(`it was true`);
-} else {
-    console.log(`it was false`);
-}
+// let T = true
+
+// if (T == true) {
+//     console.log(`its true!`);
+// }
+
+// if (T == false) {
+//     console.log(`its false!`);
+
+// }
+
+// if (T) console.log(`it was true`);
+// if ("") console.log(`this is false`);
+
+
+// if (T) {
+//     console.log(`it was true`);
+// } else {
+//     console.log(`it was false`);
+// }
 
 // let question1 = prompt("Is minecraft better than fortnight (Yes or No)")
 // let question2 = prompt("What is 93 + 7")
@@ -90,25 +90,25 @@ if (T) {
 
 // let result = (a + b < 4) ? "Below" : "Over";
 
-let message;
-let login = "Director";
+// let message;
+// let login = "Director";
 
-switch (login) {
-    case "Employee":
-        message = 'Hello';
-        break;
-    case 'Director':
-        message = 'Greetings';
-        break;
-    case '':
-        message = 'No login';
-        break;
-    default:
-        message = '';
-        break;
-}
+// switch (login) {
+//     case "Employee":
+//         message = 'Hello';
+//         break;
+//     case 'Director':
+//         message = 'Greetings';
+//         break;
+//     case '':
+//         message = 'No login';
+//         break;
+//     default:
+//         message = '';
+//         break;
+// }
 
-console.log(message);
+// console.log(message);
 
 
 // if (employee == true) {
@@ -122,47 +122,117 @@ console.log(message);
 //     }
 // }
 
-let time = 14;
-let weekdays = false;
+// let time = 14;
+// let weekdays = false;
 
-if(weekdays == false || time < 9 || time < 17){
-    open = false;
-} else{
-    open = true;
-}
+// if(weekdays == false || time < 9 || time < 17){
+//     open = false;
+// } else{
+//     open = true;
+// }
 
-console.log(open);
+// console.log(open);
 
 
 
-let flatTire = false;
-let engineLight = false;
-let milesDriven = 3145;
-if(flatTire == true || engineLight == true || milesDriven > 30000){
-    console.log(`Fix car`);
+// let flatTire = false;
+// let engineLight = false;
+// let milesDriven = 3145;
+// if(flatTire == true || engineLight == true || milesDriven > 30000){
+//     console.log(`Fix car`);
     
-}else{
-    console.log(`Car is fine`);
+// }else{
+//     console.log(`Car is fine`);
     
-}
+// }
 
-let pen = true
-let paper = false
-let mouse = true
-let keyboard = true
+// let pen = true
+// let paper = false
+// let mouse = true
+// let keyboard = true
 
-if((pen && paper) || (mouse && keyboard)){
-    console.log(`You're good to go!`);
+// if((pen && paper) || (mouse && keyboard)){
+//     console.log(`You're good to go!`);
     
-}else{
-    console.log(`You forgot something`);
+// }else{
+//     console.log(`You forgot something`);
     
-}
+// }
 
-let upLate = true;
-let manyGames = true;
-let breakfastSkipped = true;
-if(!upLate && !manyGames && !breakfastSkipped){
-    console.log(`pass test`);
+// let upLate = true;
+// let manyGames = true;
+// let breakfastSkipped = true;
+// if(!upLate && !manyGames && !breakfastSkipped){
+//     console.log(`pass test`);
+    
+// }
+
+
+// for(let i = 0; i <= 5; i++){
+//     console.log(`${i*2 + 5}`);
+
+// }
+
+// for(i = 5; i <= 11; i++){
+//     console.log(`${i}`);
+
+// }
+
+// for(i = 0; i <= 9; i++){
+//     console.log(`This is nice`);
+    
+// }
+
+// for(i = 11; i >= 1; i -= 2){
+//     console.log(`${i}`);
+    
+// }
+
+// let number
+// let string = ``
+// for(i = 0; i > 5; i++){
+    
+//     number += Math.floor(Math.random() * 101) * i
+//     console.log(i);
+    
+//     string += `${i}, `
+// }
+// console.log(string);
+
+
+
+// let sum = 0;
+// let amount = 0;
+
+// while(true) {
+//     let value = +prompt(`Enter a test score`, `Leave blank if done`);
+//     if (!value) break;
+//     sum += value;
+//     amount++;
+// }
+
+// let average = sum / amount;
+// let grade = ""
+// if (average >= 90){
+//     grade = "A"
+// }else if(average >= 80){
+//     grade = "B"
+// }else if(average >= 80){
+//     grade = "C"
+// }else if(average >= 80){
+//     grade = "D"
+// }else{
+//     grade = "F"
+// }
+
+// console.log(`Sum: ${sum}`);
+// console.log(`Amount: ${amount}`);
+// console.log(`Average: ${average}`);
+// console.log(`Grade: ${grade}`);
+
+
+for(let i = 0; i < 100; i++){
+    if(i % 5 == 0) continue;
+    console.log(i);
     
 }
