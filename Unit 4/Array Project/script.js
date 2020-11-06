44d65e464caf9ad3f8c54f8b4f7af3223bb07364
Jhunt <jhunt646@west-mec.org>
2020-11-05 21:49:25 -0700

    exhoost fume

--- Unit 4/Array Project/script.js
+++ Unit 4/Array Project/script.js
@@ -80,3 +80,78 @@
 // 	direction = "same location"
 // }
 // console.log(`${Math.abs(west - east)} stations to the ${direction}`)
+
+
+//!BRAINDEAD
+
+// let string = "()))(()()()()())()(((()()()()()(()()()())()()()((()()()())()()()()((()()()";
+// let west = 0;
+// let east = 0;
+// let direction = "";
+// let arr = string.match(/.{1,4}/g);
+// let temp = 0
+
+
+// for (i of arr) {
+// 	let value = 0;
+// 	let binary = [0, 0, 0];
+// 	let splitArr = arr[temp].split("")
+// 	if (splitArr[0] == "(") {
+// 		console.log(splitArr)
+// 		if (splitArr[1] == "(") {
+// 			binary[0] = 1
+// 		}
+// 		if (splitArr[2] == "(") {
+// 			binary[1] = 1
+// 		}
+// 		if (splitArr[3] == "(") {
+// 			binary[2] = 1
+// 		}
+// 		if (binary[0] == 1) {
+// 			value += 4
+// 		}
+// 		if (binary[1] == 1) {
+// 			value += 2
+// 		}
+// 		if (binary[2] == 1) {
+// 			value += 1
+// 		}
+// 		west += value
+// 	} else if (splitArr[0] == ")") {
+// 		console.log(splitArr)
+// 		if (splitArr[1] == "(") {
+// 			binary[0] = 1
+// 		}
+// 		if (splitArr[2] == "(") {
+// 			binary[1] = 1
+// 		}
+// 		if (splitArr[3] == "(") {
+// 			binary[2] = 1
+// 		}
+// 		if (binary[0] == 1) {
+// 			value += 4
+// 		}
+// 		if (binary[1] == 1) {
+// 			value += 2
+// 		}
+// 		if (binary[2] == 1) {
+// 			value += 1
+// 		}
+// 		east += value
+// 	}
+// temp += 1
+// console.log(value)
+// console.log(binary)
+// }
+
+// if(west > east){
+// 	direction = "west"
+// }else if(east > west){
+// 	direction = "east"
+// }else if(west == east){
+// 	direction = "same location"
+// }
+
+// console.log(`${Math.abs(west - east)} stations to the ${direction}`)
+
+
