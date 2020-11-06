9574b75e7354f7f9e1a36d34d5e4c83d4a360845
Jhunt <jhunt646@west-mec.org>
2020-11-05 18:27:07 -0700

    Hi

---
+++ Unit 4/Array Project/script.js
@@ -0,0 +1,82 @@
+//!THIS IS THE FIRST ONE
+
+// let string = "((()"
+// let west = 0;
+// let east = 0;
+// let direction = "";
+// let arr = string.split("")
+
+
+// for(i of arr){
+// 	if(i == "("){
+// 		west += 1
+// 	}else if(i == ")"){
+// 		east += 1
+// 	}
+// }
+
+// if(west > east){
+// 	direction = "west"
+// }else if(east > west){
+// 	direction = "east"
+// }else if(west == east){
+// 	direction = "same location"
+// }
+// console.log(`${Math.abs(west - east)} stations to the ${direction}`)
+
+
+
+//!THIS IS THE BUMP ONE
+
+// let string = "((()"
+// let west = 0;
+// let east = 0;
+// let direction = "";
+// string = string.replace("((", "")
+// string = string.replace("))", "")
+// let arr = string.split("")
+
+
+// for(i of arr){
+// 	if(i == "("){
+// 		west += 1
+// 	}else if(i == ")"){
+// 		east += 1
+// 	}
+// }
+
+// if(west > east){
+// 	direction = "west"
+// }else if(east > west){
+// 	direction = "east"
+// }else if(west == east){
+// 	direction = "same location"
+// }
+// console.log(`${Math.abs(west - east)} stations to the ${direction}`)
+
+
+//!WEST BRIDGE BROKEN ONE
+
+// let string = "()()())()()()()()()()()()()()())(()()()()()()()"
+// let west = 0;
+// let east = 0;
+// let direction = "";
+// let arr = string.split("")
+
+
+// for(i of arr){
+// 	if(i == "(" && east > west){
+// 		west += 1
+// 	}else if(i == ")"){
+// 		east += 1
+// 	}
+// }
+
+// if(west > east){
+// 	direction = "west"
+// }else if(east > west){
+// 	direction = "east"
+// }else if(west == east){
+// 	direction = "same location"
+// }
+// console.log(`${Math.abs(west - east)} stations to the ${direction}`)
